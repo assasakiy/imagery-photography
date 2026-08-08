4906651a6fd59729119759bab9d67764f60450dd
assasakiy <assasakiy@users.noreply.github.com>
2026-08-08 17:41:09 +0000

    remove stray temp file

--- mk.php
+++
@@ -1,8 +0,0 @@
-<?php
-require '/var/www/imagery/vendor/autoload.php';
-$app=require '/var/www/imagery/bootstrap/app.php'; $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-use App\Models\ClientAccessToken;
-use App\Models\Project;
-$p=Project::create(['user_id'=>3,'name'=>'Test Arsip Verify','status'=>'awaiting_payment','price'=>1500000,'preview_ends_at'=>now()->addDays(20)]);
-ClientAccessToken::create(['project_id'=>$p->id,'user_id'=>3,'token'=>ClientAccessToken::generateToken(),'expires_at'=>now()->addYear()]);
-echo $p->id;
